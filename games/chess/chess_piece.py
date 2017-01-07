d09ce51e0795afb68391de3ccb018d4b17a4a518
spode021 <spode021@d.umn.edu>
2017-01-07 12:57:28 -0600

    added init and tested branch

--- games/chess/chess_piece.py
+++ games/chess/chess_piece.py
@@ -12,9 +12,19 @@ class Chess_Piece:
     # Y coordinate on board
     y_position
 
+	
+	def __init__(self):
+		x_position = -1
+		y_position = -1
+		piece_type = 'NOTYPE'
+		
     # Populates the chess piece with information. Used at the start of
     # the game to initialize all of the pieces
     def Populate_Piece(x_pos, y_pos, p_type):
         x_position = x_pos
         y_position = y_pos
         piece_type = p_type
+		
+
+	
+	
